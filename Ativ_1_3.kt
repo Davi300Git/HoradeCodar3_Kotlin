2bf6cd5fddb5a7b0a45d071f88ad126a8c9fa74f
Davi Sandro dos Santos <93402520+Davi300Git@users.noreply.github.com>
2025-03-19 12:30:54 -0300

    Update Ativ_1_3.kt

--- Ativ_1_3.kt
+++ Ativ_1_3.kt
@@ -3,11 +3,7 @@ fun main() {
 
     for (i in 30 downTo 0) {
         println(i) // Exibe o número atual da contagem
-        try {
-            Thread.sleep(900)
-        } catch (e: InterruptedException) {
-            e.printStackTrace() // Trata possíveis erros durante a pausa
-        }
+            Thread.sleep(1000)
     }
     println("EXPLOSÃO!!!")
 }
